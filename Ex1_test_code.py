03cc6d61a0760b75321b0600ebc5e644fa40c966
BethEvansNHSE <bethan.evans15@nhs.net>
2024-09-17 14:12:34 +0100

    Ex1_test_code.py: function to divide numbers

--- Ex1_test_code.py
+++ Ex1_test_code.py
@@ -5,3 +5,7 @@ def add_numbers(num1, num2):
 def subtract_numbers(num1, num2):
     result = num1 - num2
     return result
+
+def divide_numbers(num1, num2):
+    result = num1 / num2
+    return result
