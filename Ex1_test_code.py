c823582569b4a440e477a58a476ee12798cdf19c
BethEvansNHSE <bethan.evans15@nhs.net>
2024-09-17 14:22:21 +0100

    Ex1_test_code: blah

--- Ex1_test_code.py
+++ Ex1_test_code.py
@@ -6,6 +6,9 @@ def subtract_numbers(num1, num2):
     result = num1 - num2
     return result
 
+def print_text(blah):
+    print(blah)
+
 def divide_numbers(num1, num2):
     result = num1 / num2
     return result
